09f3fc81da95fcc818e1ed46d882b8df89595ea4
Akil Burgess <akilburgess@gmail.com>
2017-08-12 07:29:07 +0100

    Try to use AppSettings first to get settings
    CloudConfigurationManager wasn't designed for use in Azure App Service so we just use it as a fallback since it works locally. See https://stackoverflow.com/a/44788464

--- RukatukApi/Services/Configuration.cs
+++ RukatukApi/Services/Configuration.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.Azure;
 using System;
+using System.Configuration;
 
 namespace RukatukApi.Services
 {
     public class Configuration : IConfiguration
     {
-        public string AzureStorageConnectionString => CloudConfigurationManager.GetSetting("AzureWebJobsStorage");
+        public string AzureStorageConnectionString => GetSetting("AzureWebJobsStorage");
 
-        public string EventbriteOAuthToken => CloudConfigurationManager.GetSetting("EventbriteOAuthToken");
+        public string EventbriteOAuthToken => GetSetting("EventbriteOAuthToken");
 
-        public string FlickrApiKey => CloudConfigurationManager.GetSetting("FlickrApiKey");
+        public string FlickrApiKey => GetSetting("FlickrApiKey");
 
-        public string FlickrApiSecret => CloudConfigurationManager.GetSetting("FlickrApiSecret");
+        public string FlickrApiSecret => GetSetting("FlickrApiSecret");
 
         public string FlickrPhotoSetId => "72157687357180146";
+
+        private string GetSetting(string name)
+        {
+            return ConfigurationManager.AppSettings[name] ?? CloudConfigurationManager.GetSetting(name);
+        }
     }
 }
